e010fa5d1ae6a6e4718ab6c1b2ac948187dcff9b
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2022-11-10 18:00:07 -0300

    corrigindo endereçamento de pastas
    em andamento

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 function createGames(player1, hour, player2) {
   return `
     <li>
-      <img src="/icones/icon-${player1}.svg" alt="bandeira do ${player1}">
+      <img src="../icones/icon-${player1}.svg" alt="bandeira do ${player1}">
       <strong>${hour}</strong>
-      <img src="/icones/icon-${player2}.svg" alt="bandeira do ${player2}">
+      <img src="../icones/icon-${player2}.svg" alt="bandeira do ${player2}">
     </li>`;
 }
 
